5697492919eb9f1124cdeb4f14a784e119a8c0ae
Azat Idrisov <azzzat72@yandex.ru>
2021-04-28 10:19:15 +0500

    0. Страница Login.jsp [#495358]

--- src/main/java/ru/job4j/dream/servlet/AuthServlet.java
+++ src/main/java/ru/job4j/dream/servlet/AuthServlet.java
@@ -22,7 +22,6 @@ public class AuthServlet extends HttpServlet {
             sc.setAttribute("user", admin);
             resp.sendRedirect(req.getContextPath() + "/posts.do");
         } else {
-            req.setAttribute("error", "Не верный email или пароль");
             req.getRequestDispatcher("login.jsp").forward(req, resp);
         }
     }
