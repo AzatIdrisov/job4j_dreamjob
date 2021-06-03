14d3e47232da177877d3b58ce2d47a70f139e9e5
Azat Idrisov <azzzat72@yandex.ru>
2021-06-03 12:33:04 +0500

    1. Написать тест на PostServlet. [#2518 #38886] add doGetTest

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -7,13 +7,16 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import ru.job4j.dream.model.Post;
+import ru.job4j.dream.model.User;
 import ru.job4j.dream.store.MemStore;
 import ru.job4j.dream.store.PsqlStore;
 import ru.job4j.dream.store.Store;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -38,7 +41,7 @@ public class PostServletTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        assertThat (MemStore.instOf().findPostById(4), is(new Post(4,"Test")));
+        assertThat(MemStore.instOf().findPostById(4), is(new Post(4, "Test")));
     }
 
     @Test
@@ -55,7 +58,27 @@ public class PostServletTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        assertThat (MemStore.instOf().findPostById(1).getName(), is("Test Update"));
+        assertThat(MemStore.instOf().findPostById(1).getName(), is("Test Update"));
     }
 
+    @Test
+    public void doGetTest() throws ServletException, IOException {
+        User user = new User (1, "Admin", "root@local", "root");
+        Store store = MemStore.instOf();
+        PowerMockito.mockStatic(PsqlStore.class);
+        Mockito.when(PsqlStore.instOf()).thenReturn(store);
+        HttpSession session = Mockito.mock(HttpSession.class);
+        RequestDispatcher dispatcher = Mockito.mock(RequestDispatcher.class);
+        HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse resp = Mockito.mock(HttpServletResponse.class);
+        Mockito.when(req.getSession()).thenReturn(session);
+        Mockito.when(session.getAttribute("user")).thenReturn(user);
+        Mockito.when(req.getRequestDispatcher("post/posts.jsp")).thenReturn(dispatcher);
+        try {
+            new PostServlet().doGet(req, resp);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Mockito.verify(req).getRequestDispatcher("post/posts.jsp");
+    }
 }
